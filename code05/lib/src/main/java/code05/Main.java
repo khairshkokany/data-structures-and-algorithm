e1fdb9bdb4f4f7e5002c42928f9bb844f8a397a9
khair eldeen zakaria shkokany <shkokany98@gmail.com>
2021-09-30 22:54:14 +0300

    finished code 05

---
+++ code05/lib/src/main/java/code05/Main.java
@@ -0,0 +1,21 @@
+package code05;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println("hello from package");
+
+
+    LinkedList linkedList = new LinkedList();
+        linkedList.add("K");
+        linkedList.add("H");
+        linkedList.add("A");
+        linkedList.add("I");
+        linkedList.add("R");
+
+        System.out.println(linkedList.included("R"));
+        linkedList.printList();
+
+    }
+}
